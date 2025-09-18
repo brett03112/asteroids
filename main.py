fb7779b1227be9e3e6f424ddc1ce3ccdc4dd3a72
Brett Smith <brett03112@gmail.com>
2025-09-18 18:49:57 -0400

    CH2: Player . L3: FPS

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True: # Main game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, ("black"))
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
